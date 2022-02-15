205269391c11cf87b2b966697cf0eeb4419b6f4e
Chris Bull <christopher.bull@dcmail.ca>
2022-02-15 05:01:29 -0500

    Updated custom theme files
    Replaced _s in files with "compextra" theme slug

--- themes/compextra/compextra/functions.php
+++ themes/compextra/compextra/functions.php
@@ -7,9 +7,9 @@
  * @package Computer_Extras
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'COMPEXTRA_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'COMPEXTRA_VERSION', '1.0.0' );
 }
 
 /**
@@ -138,10 +138,10 @@ add_action( 'widgets_init', 'compextra_widgets_init' );
  * Enqueue scripts and styles.
  */
 function compextra_scripts() {
-	wp_enqueue_style( 'compextra-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'compextra-style', get_stylesheet_uri(), array(), COMPEXTRA_VERSION );
 	wp_style_add_data( 'compextra-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'compextra-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'compextra-navigation', get_template_directory_uri() . '/js/navigation.js', array(), COMPEXTRA_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- themes/compextra/compextra/inc/customizer.php
+++ themes/compextra/compextra/inc/customizer.php
@@ -56,6 +56,6 @@ function compextra_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function compextra_customize_preview_js() {
-	wp_enqueue_script( 'compextra-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'compextra-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), COMPEXTRA_VERSION, true );
 }
 add_action( 'customize_preview_init', 'compextra_customize_preview_js' );
