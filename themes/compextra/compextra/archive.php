49c6daedb3f08e5ed4ac9daae57c1729bfeb8fbc
Chris Bull <christopher.bull@dcmail.ca>
2022-03-04 23:53:19 -0500

    Removed page title display from blog page

--- themes/compextra/compextra/archive.php
+++ themes/compextra/compextra/archive.php
@@ -13,14 +13,6 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
