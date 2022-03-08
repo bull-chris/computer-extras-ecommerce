9038ad9ed47d7f1e3377a23ee82b24fff6c0e220
Chris Bull <christopher.bull@dcmail.ca>
2022-03-08 15:37:04 -0500

    Final comments added to woocommerce.php and compextra-styling files

--- themes/compextra/compextra/inc/woocommerce.php
+++ themes/compextra/compextra/inc/woocommerce.php
@@ -1,9 +1,11 @@
 <?php
 /* File for editing layout of woocommerce pages */
 
+//remove catalog ordering dropdown menu and results count from shop page
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 
+//function to add the categories menu to the shop page
 function compextra_add_store_menu() {
     ?>
     <div class="store-categories">
@@ -20,7 +22,9 @@ function compextra_add_store_menu() {
     <?php
 }
 
+//add the categories menu to the shop page
 add_action( 'woocommerce_before_shop_loop', 'compextra_add_store_menu', 30 );
 
+//remove the product price and re-add it before the product image thumbnail on the product page
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 add_action( 'woocommerce_before_single_product_summary', 'woocommerce_template_single_price', 4 );
